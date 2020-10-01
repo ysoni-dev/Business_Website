5031e81a52d1b5f9e2507f3d6696e5dd9a303cd7
ysoni-dev <ysoni1614@gmail.com>
2020-10-01 11:18:35 +0530

    Updates

---
+++ precache-manifest.7c3cd18f96fc68a746692e39948b4de3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612a10892b4f91b462048e17569f25b5",
+    "url": "/React-project/index.html"
+  },
+  {
+    "revision": "9eb5e2f06b3ef4175cee",
+    "url": "/React-project/static/css/2.92258400.chunk.css"
+  },
+  {
+    "revision": "06b6298c762fdc9d1560",
+    "url": "/React-project/static/css/main.ec53c645.chunk.css"
+  },
+  {
+    "revision": "9eb5e2f06b3ef4175cee",
+    "url": "/React-project/static/js/2.1483f89e.chunk.js"
+  },
+  {
+    "revision": "e60f6644a238030ec5fb7683004cdede",
+    "url": "/React-project/static/js/2.1483f89e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b6298c762fdc9d1560",
+    "url": "/React-project/static/js/main.3cde3d2d.chunk.js"
+  },
+  {
+    "revision": "5a541d56299de1bb4b90",
+    "url": "/React-project/static/js/runtime-main.76175aa3.js"
+  },
+  {
+    "revision": "4160894a92adbadf75a81e9230fa91bb",
+    "url": "/React-project/static/media/Birds-Flourish.4160894a.svg"
+  },
+  {
+    "revision": "ea74ed5ea826a647a349861a1e49d337",
+    "url": "/React-project/static/media/Floral-Flourish-5.ea74ed5e.svg"
+  },
+  {
+    "revision": "c7368f11aab23b55f30162a85ebfc3b4",
+    "url": "/React-project/static/media/HelloStockholm-Regular.c7368f11.ttf"
+  },
+  {
+    "revision": "36d07e5cd715aaeb6f9e1a320d1a5c0c",
+    "url": "/React-project/static/media/Hero-Light.36d07e5c.ttf"
+  },
+  {
+    "revision": "cd643f819ae037281b54fcfaf62dc4a8",
+    "url": "/React-project/static/media/Untitled.cd643f81.jpg"
+  },
+  {
+    "revision": "c186db3b4cc60f62da5d657fbbe4aa15",
+    "url": "/React-project/static/media/Untitled1.c186db3b.jpg"
+  },
+  {
+    "revision": "31b35b55535ae5428586700226ee9fa7",
+    "url": "/React-project/static/media/Untitled2.31b35b55.jpg"
+  },
+  {
+    "revision": "ae1c75e21df0e7b2ff12da060632fbc7",
+    "url": "/React-project/static/media/about1.ae1c75e2.png"
+  },
+  {
+    "revision": "c0309b4b178f529b0a64df2eeeaa5453",
+    "url": "/React-project/static/media/bakery.c0309b4b.mp4"
+  },
+  {
+    "revision": "77259ab4f8aa2f9533a2b069a434dc46",
+    "url": "/React-project/static/media/business1.77259ab4.png"
+  },
+  {
+    "revision": "88e5bb140272509054b34043e27ac172",
+    "url": "/React-project/static/media/choco1.88e5bb14.jpg"
+  },
+  {
+    "revision": "f28d545efa358437a88fe052087779c4",
+    "url": "/React-project/static/media/cool.f28d545e.jpg"
+  },
+  {
+    "revision": "c03dc8dfbc8d323e4231df21bc66818d",
+    "url": "/React-project/static/media/cool1.c03dc8df.jpg"
+  },
+  {
+    "revision": "2ff4bc1c1c130cd2b8a0bba7cded3d1a",
+    "url": "/React-project/static/media/four.2ff4bc1c.jpg"
+  },
+  {
+    "revision": "db21e780d7879c2e25c113fa63ad90c4",
+    "url": "/React-project/static/media/furniture.db21e780.mp4"
+  },
+  {
+    "revision": "37fd1d612e5a2f8a98cf3abf091f1a34",
+    "url": "/React-project/static/media/hand_pizza7.37fd1d61.mp4"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ba2fb74b53e46a1a394d06958f44fa1",
+    "url": "/React-project/static/media/one.5ba2fb74.jpg"
+  },
+  {
+    "revision": "52703dc774a9777b858869a168983f1b",
+    "url": "/React-project/static/media/roboto-bold-webfont.52703dc7.woff"
+  },
+  {
+    "revision": "d04709efeec55f551ec1f24cdd5166da",
+    "url": "/React-project/static/media/roboto-bold-webfont.d04709ef.woff2"
+  },
+  {
+    "revision": "cfe094b65a42efc4d413c7d0d99bfdae",
+    "url": "/React-project/static/media/two.cfe094b6.jpg"
+  }
+]);
